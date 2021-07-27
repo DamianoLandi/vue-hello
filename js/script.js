d6cfdb9c5c3e6116c73d904e772c0759a203ec5e
DamianoLandi <damianolandi01@gmail.com>
2021-07-27 16:38:54 +0200

    IMG input

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Vue.config.devtools = true;
 var root = new Vue({
     el: '#root',
     data: {
-      message: 'Hello World!'
+      message: 'Hello World!',
+      imgSrc: "https://i.guim.co.uk/img/media/6088d89032f8673c3473567a91157080840a7bb8/413_955_2808_1685/master/2808.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=412cc526a799b2d3fff991129cb8f030",  
     }
   });
-
